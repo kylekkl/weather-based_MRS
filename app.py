445f052e70e3531e64e70cc9fcf085f9e9b03011
kylekkl <151366906+kylekkl@users.noreply.github.com>
2024-01-10 20:35:22 +1100

    Add files via upload

--- app.py
+++ app.py
@@ -10,21 +10,15 @@ from youtube import Youtube
 
 # Constants and Global Variables
 WEATHER_API = "adea9ed55a37f7a427463f3cde699948"
-YOUTUBE_API = "AIzaSyBlGzM3gF5929_bH4lCrG88sTn42kN4OrQ"
-#AIzaSyDrte07cVXGrqHB_iawwowsU-Sv39JzHdE
-#AIzaSyDzWCrerZ-eMXbuqhTldKNgsiyaDUAcEW4
-#AIzaSyBlGzM3gF5929_bH4lCrG88sTn42kN4OrQ
+YOUTUBE_API = "AIzaSyDzWCrerZ-eMXbuqhTldKNgsiyaDUAcEW4"
 MONGODB_URL = "mongodb+srv://kylekakili:kBdqAZk5IaYPiyPO@cluster0.b7zpfgx.mongodb.net/"
-USER_SESSION = {
-    "location": "",
-    "mood": "",
-    "songs": [],
-    "amount_of_like": 0,
-    "recommended_songs": set(),
-    
-}
+# AIzaSyDrte07cVXGrqHB_iawwowsU-Sv39JzHdE
+# AIzaSyDzWCrerZ-eMXbuqhTldKNgsiyaDUAcEW4
+# AIzaSyBlGzM3gF5929_bH4lCrG88sTn42kN4OrQ
+USER_SESSION = {}
 TEMP_SONG = {}
-
+recommended = []
+last_recommended_artist = None
 
 # Flask App Initialization
 app = Flask(__name__)
@@ -66,7 +60,6 @@ def handle_city_input():
     location = request.form.get("city", "").title()  # Get location from user
     if not location:
         return render_template("index.html", warning="Please enter your current city.")
-
     weather = Weather(api_key=WEATHER_API, city=location)
     (
         temperature,
@@ -82,7 +75,6 @@ def handle_city_input():
         "amount_of_like": 0,
     }
     collection.insert_one(USER_SESSION)
-    print(location, mood, weather_condition, temperature)
     if mood:
         SONG_LIST = df[
             df["mood"] == mood
@@ -96,7 +88,7 @@ def handle_city_input():
 
 
 def handle_reaction():
-    global USER_SESSION, SONG_LIST
+    global USER_SESSION, SONG_LIST, TEMP_SONG, last_recommended_artist, recommended
     rating = request.form.get(
         "reaction", "Not Rated"
     )  # Set to default 'Not Rated' if reaction is not found, if user press like, rating = 'like'
@@ -112,23 +104,41 @@ def handle_reaction():
     if (
         USER_SESSION["amount_of_like"] >= 3
     ):  # At least liked 3 songs, then run the genre based
-        return recommend_based_on_genre()
-    else:
-        if SONG_LIST is not None and not SONG_LIST.empty:
-            video_id = (
-                get_song_video_id()
-            )  # Else keep using the first dataset with the mood
-            return render_template("index.html", video_id=video_id)
-    return render_template("index.html")
+        data = collection.find_one({"_id": USER_SESSION["_id"]})
+        liked_songs = [song for song in data["songs"] if song.get("rating") == "like"]
+        liked_songs_df = pd.DataFrame(liked_songs)
+        liked_songs_df["genre_combined"] = liked_songs_df["musicGenre"].apply(
+            lambda x: " ".join(x.split(", "))
+        )
+        recommendations = generate_playlist_recos(genre_based_df, liked_songs_df)
+        if not recommendations.empty:
+            for _, top_song in recommendations.iterrows():
+                song_identifier = (top_song["name"], top_song["artist"])
+                current_artist = top_song["artist"]
+                if (
+                    current_artist != last_recommended_artist
+                    and song_identifier not in recommended
+                ):
+                    recommended.append(song_identifier)
+                    last_recommended_artist = current_artist
+                    song_name = top_song["name"]
+                    artist_name = top_song["artist"]
+                    TEMP_SONG = {
+                        "nameOfSong": top_song["name"],
+                        "artist": top_song["artist"],
+                        "musicGenre": top_song["genre"],
+                    }
+                    search_query = f"{artist_name} - {song_name}"
+                    yt = Youtube(api_key=YOUTUBE_API, search_query=search_query)
+                    video_id = yt.get_result()
+                    if video_id:
+                        return render_template("index.html", video_id=video_id)
+                    break
+    return render_template("index.html", video_id=get_song_video_id())
 
-
-recommended = []
-
-
-last_recommended_artist = None
 
 def get_song_video_id():
-    global SONG_LIST, TEMP_SONG, last_recommended_artist
+    global SONG_LIST, TEMP_SONG, last_recommended_artist, recommended
     if SONG_LIST is not None and not SONG_LIST.empty:
         while True:
             random_row = SONG_LIST.sample()  # Get a random row of the song list
@@ -136,67 +146,60 @@ def get_song_video_id():
                 random_row["name"].iloc[0],
                 random_row["artist"].iloc[0],
             )  # Unique identifier for the song
-
             current_artist = random_row["artist"].iloc[0]
-
             # Check if the current artist is the same as the last recommended artist
-            if current_artist != last_recommended_artist and song_identifier not in recommended:
+            if (
+                current_artist != last_recommended_artist
+                and song_identifier not in recommended
+            ):
                 recommended.append(song_identifier)
-                last_recommended_artist = current_artist  # Update the last recommended artist
+                last_recommended_artist = (
+                    current_artist  # Update the last recommended artist
+                )
 
                 TEMP_SONG = {
-                    "nameOfSong": random_row["name"].to_string(index=False),
-                    "artist": current_artist,
-                    "musicGenre": random_row["genre"].to_string(index=False),
+                    "nameOfSong": random_row["name"].to_string(index=False).strip(),
+                    "artist": random_row["artist"].to_string(index=False).strip(),
+                    "musicGenre": random_row["genre"].to_string(index=False).strip(),
                 }
                 search_query = f"{TEMP_SONG['artist']} - {TEMP_SONG['nameOfSong']}"
                 yt = Youtube(api_key=YOUTUBE_API, search_query=search_query)
                 return yt.get_result()
 
-            if len(recommended) >= len(SONG_LIST):
-                # All songs have been recommended, handle this case
-                return None
-
-
 
 """Update Database"""
+
+
 def update_user_session(song_info):
     global USER_SESSION
     collection.update_one({"_id": USER_SESSION["_id"]}, {"$push": {"songs": song_info}})
 
 
-def recommend_based_on_genre():
-    global USER_SESSION, SONG_LIST, tfidf_vectorizer, tfidf_matrix, genre_based_df
-    user_preferences = collection.find_one({"_id": USER_SESSION["_id"]})
+def create_feature_set(df):
+    tfidf = TfidfVectorizer()
+    tfidf_matrix = tfidf.fit_transform(df["genre_combined"])
+    genre_df = pd.DataFrame(tfidf_matrix.toarray())
+    genre_df.columns = ["genre" + "|" + i for i in tfidf.get_feature_names_out()]
 
-    liked_genres = set()
-    for song in user_preferences["songs"]:
-        if song["rating"] == "like":
-            genres = song["musicGenre"].split(", ")
-            liked_genres.update(genres)
+    return genre_df
 
-    user_genres_combined = " ".join(liked_genres)
-    user_tfidf_vector = tfidf_vectorizer.transform([user_genres_combined])
-    cosine_similarities = cosine_similarity(user_tfidf_vector, tfidf_matrix)
 
-    sorted_indices = np.argsort(cosine_similarities[0])[::-1]
-    previously_recommended_songs = {
-        song["nameOfSong"] for song in user_preferences["songs"]
-    }
+def generate_playlist_recos(df, user_df):
+    df_features = create_feature_set(df)
+    user_features = create_feature_set(user_df)
 
-    for idx in sorted_indices:
-        recommended_song = genre_based_df.iloc[idx]
-        if recommended_song["name"] not in previously_recommended_songs:
-            SONG_LIST = recommended_song.to_frame().T
-            video_id = get_song_video_id()
-            if video_id is not None:
-                return render_template("index.html", video_id=video_id)
+    # Ensure both feature sets have the same columns
+    common_columns = df_features.columns.intersection(user_features.columns)
+    df_features = df_features[common_columns]
+    user_features = user_features[common_columns]
 
-    # If no new songs are found
-    return render_template(
-        "index.html", warning="No new songs found based on your preferences."
-    )
+    # Calculate similarity
+    df["sim"] = cosine_similarity(
+        df_features, user_features.mean().values.reshape(1, -1)
+    )[:, 0]
+    top_recommendation = df.sort_values("sim", ascending=False).head(5)
 
+    return top_recommendation
 
 
 if __name__ == "__main__":
